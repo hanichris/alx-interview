e708c11e59f966d7a40bb332e746a922a8f065e3
hanichris <hanichris71@gmail.com>
2023-03-11 20:38:11 +0300

    process command line inputs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const movieId = process.argv[2]
+if (process.argv.length == 2) {
+  console.error('Expected at least one arguments');
+  process.exit(1);
+}
+if (process.argv.length > 3) {
+  console.error('Usage: ./0-starwars_characters.js N');
+  process.exit(1);
+}
