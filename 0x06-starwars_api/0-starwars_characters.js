18f312461e08006749de06877c7ccd47d7c15b99
hanichris <hanichris71@gmail.com>
2023-03-11 20:40:17 +0300

    Check the number of command-line inputs sent to the script.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-if (process.argv.length == 2) {
-  console.error('Expected at least one arguments');
-  process.exit(1);
-}
-if (process.argv.length > 3) {
+if (process.argv.length != 3) {
   console.error('Usage: ./0-starwars_characters.js N');
   process.exit(1);
 }
