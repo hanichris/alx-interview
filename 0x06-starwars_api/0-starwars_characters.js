8f48fd644d9b0835688983eef3d74aaece252cec
hanichris <hanichris71@gmail.com>
2023-03-11 21:06:05 +0300

    make a get request to an api.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-if (process.argv.length != 3) {
+
+if (process.argv.length !== 3) {
   console.error('Usage: ./0-starwars_characters.js N');
   process.exit(1);
 }
+
+const movieId = process.argv[2]
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (err, res, body) => {
+  console.log(body);
+})
