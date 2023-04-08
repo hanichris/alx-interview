1c81eb3beda5d86481d6d909f9ccf6e4da73fc0c
hanichris <hanichris71@gmail.com>
2023-04-08 19:47:33 +0300

    Test whether an empty list of numbers was passed to the function.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -33,6 +33,8 @@ def isWinner(x, nums):
     Return:
         string: name of individual who won (either Ben or Maria).
     """
+    if nums is None or nums == []:
+        return None
     benCount = mariaCount = 0
 
     for i in range(x):
